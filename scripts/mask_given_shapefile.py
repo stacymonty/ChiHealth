60d20ed1bf125a578c7bcf831d33cb0406b201c6
Anastasia (Stacy) Montgomery <40803590+stacymonty@users.noreply.github.com>
2023-03-10 15:55:12 -0600

    Create mask_given_shapefile.py

---
+++ scripts/mask_given_shapefile.py
@@ -0,0 +1,18 @@
+def mask_given_shapefile(lon,lat,shapefile):
+   import geopandas
+   from shapely.ops import unary_union 
+   ‘’'
+   Make a mask given a shapefile
+   lon - array of grid lons
+   lat - array of grid lats
+   shapefile - geopandas geodataframe of a shapefile, needs geometry column
+   ‘’'
+   union=gpd.GeoSeries(unary_union(shapefile.geometry))
+   mask=np.ones(lon.shape,dtype=bool)
+   mask[:] = False
+   for i in range(len(lon)):
+      for j in range(len(lon[0])):
+         pt = Point(lon[i][j],lat[i][j])
+         mask[i][j] =  pt.within(union[0])
+   #
+   return mask
